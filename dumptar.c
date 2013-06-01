054612a512cc2b2b1de18f9b50a644f856c0bfb7
Stefan Edwards <saedwards.ecc@gmail.com>
2013-05-31 22:34:43 -0400

    actually needed a tar metadata dumper, so I finally got around to fixing dumptar (probably do not want to ask)

--- dumptar.c
+++ dumptar.c
@@ -14,7 +14,7 @@
 int main(int ac,char **al)
 {
 	char file[512];
-	int iter = 0;
+	int iter = 0, size = 0;
 	FILE *fdin = NULL;
 	if(ac == 1)
 		fdin = stdin;
@@ -26,43 +26,58 @@ int main(int ac,char **al)
 			return 2;
 		}
 	}
-	if(fread(file,sizeof(char),512,fdin) < 512)
-	{
-		fprintf(stderr,"Invalid tar archive!\n");
-		return 1;;
-	}
-	printf("dump tar meta-info\n");
-	printf("filename: %s\n",file);
-	printf("type flag: %i\n",file[156]);
- 	printf("file mode: ");
-	for(iter = 157;iter < 257;iter++)
-	{	
-		if(!isprn(file[iter]))
-			break;
-		printf("%c",file[iter]);
-	}
-	printf("\nUSTAR magic token: ");		
-	for(iter = 257;iter < 263;iter++)
-	{
-		if(!isprn(file[iter]))
-			break;
-		printf("%c",file[iter]);
-	}
-	printf("\nustar version: 0x%X 0x%X\n",file[263],file[264]);	
-	printf("user name: ");
-	for(iter = 265;iter < 297;iter++)
-	{	
-		if(!isprn(file[iter]))
-			break;
-		printf("%c",file[iter]);
-	}
-	printf("\ngroup name: ");
-	for(iter = 297;iter < 329;iter++)
-	{	
-		if(!isprn(file[iter]))
-			break;	
-		printf("%c",file[iter]);
-	}
-	printf("\n");
+    while(1)
+    {    
+        if(feof(fdin))
+            break;
+        if(fread(file,sizeof(char),512,fdin) < 512)
+        {
+            fprintf(stderr,"Invalid tar archive!\n");
+            return 1;
+        }
+        printf("dump tar meta-info\n");
+        printf("filename: %s\n",file);
+        printf("type flag: %i\n",file[156]);
+        printf("file mode: ");
+        for(iter = 157;iter < 257;iter++)
+        {	
+            if(!isprn(file[iter]))
+                break;
+            printf("%c",file[iter]);
+        }
+        printf("\nfile size: ");
+        for(iter = 124; iter < 135; iter++)
+        {
+            if(file[iter] < '0' || file[iter] > '7')
+                break;
+            size = (size << 3) + file[iter] - '0';
+        }
+        printf("%i", size);
+        printf("\nUSTAR magic token: ");		
+        for(iter = 257;iter < 263;iter++)
+        {
+            if(!isprn(file[iter]))
+                break;
+            printf("%c",file[iter]);
+        }
+        printf("\nustar version: 0x%X 0x%X\n",file[263],file[264]);	
+        printf("user name: ");
+        for(iter = 265;iter < 297;iter++)
+        {	
+            if(!isprn(file[iter]))
+                break;
+            printf("%c",file[iter]);
+        }
+        printf("\ngroup name: ");
+        for(iter = 297;iter < 329;iter++)
+        {	
+            if(!isprn(file[iter]))
+                break;	
+            printf("%c",file[iter]);
+        }
+        printf("\n");
+        printf("offset: %d\n", ((size / 512) + 1) * 512);
+        (void)fseek(fdin, ((size / 512) + 1) * 512, SEEK_CUR);
+    }
 	return 0;
 }
